dd052485736292ba7632e78f234bd6d166c7d30e
Alena Lobacheva <danarimer785@gmail.com>
2024-08-15 20:43:15 +0400

    upd

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -56,31 +56,38 @@ const stringify = (data) => {
   return data;
 };
 
-const formatAdded = (path, value) => `Property '${path}' was added with value: ${stringify(value)}`;
+const formatAdded = (path, value) =>
+  `Property '${path}' was added with value: ${stringify(value)}`;
 const formatDeleted = (path) => `Property '${path}' was removed`;
-const formatChanged = (path, oldValue, value) => `Property '${path}' was updated. From ${stringify(oldValue)} to ${stringify(value)}`;
+const formatChanged = (path, oldValue, value) =>
+  `Property '${path}' was updated. From ${stringify(oldValue)} to ${stringify(
+    value
+  )}`;
 
 const iter = (obj, path) => {
   const values = Object.values(obj);
-  return values.flatMap((node) => {
-    const { key, oldValue, value, type } = node;
-    const newPath = path === '' ? `${key}` : `${path}.${key}`;
+  return values
+    .flatMap((node) => {
+      const { key, oldValue, value, type } = node;
+      const newPath = path === '' ? `${key}` : `${path}.${key}`;
 
-    switch (type) {
-      case 'added':
-        return formatAdded(newPath, value);
-      case 'deleted':
-        return formatDeleted(newPath);
-      case 'changed':
-        return formatChanged(newPath, oldValue, value);
-      case 'hasChild':
-        return iter(value, newPath);
-      case 'unchanged':
-        return [];
-      default:
-        throw new Error('Unknown type');
-    }
-  }).filter(Boolean).join('\n');
+      switch (type) {
+        case 'added':
+          return formatAdded(newPath, value);
+        case 'deleted':
+          return formatDeleted(newPath);
+        case 'changed':
+          return formatChanged(newPath, oldValue, value);
+        case 'hasChild':
+          return iter(value, newPath);
+        case 'unchanged':
+          return [];
+        default:
+          throw new Error('Unknown type');
+      }
+    })
+    .filter(Boolean)
+    .join('\n');
 };
 
 const plain = (data) => iter(data, '');
