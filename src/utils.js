ae4d2ffa34b2d696f440954174fbb5611b6e71a9
Alena Lobacheva <danarimer785@gmail.com>
2024-08-15 21:00:36 +0400

    rafactor

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,62 @@
 import _ from 'lodash';
 
+const getChangeType = (obj1, obj2, key) => {
+  if (!Object.hasOwn(obj1, key)) return 'added';
+  if (!Object.hasOwn(obj2, key)) return 'deleted';
+  if (obj1[key] === obj2[key]) return 'unchanged';
+  return 'changed';
+};
+
+const createAddedResult = (key, value) => ({
+  key,
+  value,
+  type: 'added',
+});
+
+const createDeletedResult = (key, value) => ({
+  key,
+  value,
+  type: 'deleted',
+});
+
+const createChangedResult = (key, oldValue, value) => ({
+  key,
+  oldValue,
+  value,
+  type: 'changed',
+});
+
+const createChildResult = (key, value) => ({
+  key,
+  value: buildDiff(value[0], value[1]),
+  type: 'hasChild',
+});
+
 const buildDiff = (obj1, obj2) => {
   const sortedUnicKeys = _.sortBy(
     _.union(Object.keys(obj1), Object.keys(obj2))
   );
-  const resultObj = sortedUnicKeys.map((key) => {
+
+  return sortedUnicKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
+    const type = getChangeType(obj1, obj2, key);
 
-    if (!Object.hasOwn(obj1, key)) {
-      return { key, value: value2, type: 'added' };
+    switch (type) {
+      case 'added':
+        return createAddedResult(key, value2);
+      case 'deleted':
+        return createDeletedResult(key, value1);
+      case 'unchanged':
+        return { key, value: value1, type };
+      case 'changed':
+        return createChangedResult(key, value1, value2);
+      case 'hasChild':
+        return createChildResult(key, [value1, value2]);
+      default:
+        throw new Error('Unknown change type');
     }
-
-    if (!Object.hasOwn(obj2, key)) {
-      return { key, value: value1, type: 'deleted' };
-    }
-
-    if (value1 === value2) {
-      return { key, value: value1, type: 'unchanged' };
-    }
-
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      return { key, value: buildDiff(value1, value2), type: 'hasChild' };
-    }
-    return {
-      key,
-      oldValue: value1,
-      value: value2,
-      type: 'changed',
-    };
   });
-  return resultObj;
 };
 
 export default buildDiff;
