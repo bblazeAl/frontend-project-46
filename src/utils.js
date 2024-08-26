9ae1167d60eb9f136e80857d97376a467c353326
Alena Lobacheva <danarimer785@gmail.com>
2024-08-26 14:13:30 +0400

    upd final?

--- src/utils.js
+++ src/utils.js
@@ -9,38 +9,25 @@ const makeDifference = (obj1, obj2) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
-    const type = (() => {
-      if (!Object.hasOwn(obj1, key)) {
-        return 'added';
-      }
-      if (!Object.hasOwn(obj2, key)) {
-        return 'deleted';
-      }
-      if (value1 === value2) {
-        return 'unchanged';
-      }
-      if (typeof value1 === 'object' && typeof value2 === 'object') {
-        return 'hasChild';
-      }
-      return 'changed';
-    })();
-
-    switch (type) {
-      case 'added':
-        return { key, value: value2, type };
-      case 'deleted':
-        return { key, value: value1, type };
-      case 'unchanged':
-        return { key, value: value1, type };
-      case 'changed':
-        return {
-          key, oldValue: value1, value: value2, type,
-        };
-      case 'hasChild':
-        return { key, value: makeDifference(value1, value2), type };
-      default:
-        throw new Error('Unknown change type');
+    if (!Object.hasOwn(obj1, key)) {
+      return { key, value: value2, type: 'added' };
     }
+    if (!Object.hasOwn(obj2, key)) {
+      return { key, value: value1, type: 'deleted' };
+    }
+    if (value1 === value2) {
+      return { key, value: value1, type: 'unchanged' };
+    }
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      return { key, value: makeDifference(value1, value2), type: 'hasChild' };
+    }
+
+    return {
+      key,
+      oldValue: value1,
+      value: value2,
+      type: 'changed',
+    };
   });
 };
 
